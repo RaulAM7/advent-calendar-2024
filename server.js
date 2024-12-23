c90551a3d27e6ecf67106834a62b953ccdf5a697
raulAM7 <raulartilesm@gmail.com>
2024-12-23 18:51:20 +0000

    update

---
+++ server.js
@@ -0,0 +1,29 @@
+const bGround = require('fcc-express-bground');
+// Nos importamos los routers
+const jsfile1 = require('./route-parameters');
+const jsfile2 = require('./query-parameters');
+// Montamos el objeto app express del server
+const express = require('express');
+const app = express();
+
+// Configuramos los routers
+app.use(jsfile1);
+app.use(jsfile2);
+
+if (!process.env.DISABLE_XORIGIN) {
+  app.use((req, res, next) => {
+    const allowedOrigins = ['https://narrow-plane.gomix.me', 'https://www.freecodecamp.com'];
+    const origin = req.headers.origin || '*';
+    if(!process.env.XORIG_RESTRICT || allowedOrigins.indexOf(origin) > -1){
+         console.log(origin);
+         res.setHeader('Access-Control-Allow-Origin', origin);
+         res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    }
+    next();
+  });
+}
+
+const port = process.env.PORT || 3000;
+bGround.setupBackgroundApp(app, app, __dirname).listen(port, () => {
+  bGround.log(`Node is listening on port ${port}...`);
+});
